3a1ab641446fe739e656c9f442a1e2f893432b26
stephanietripodi63 <70589856+stephanietripodi63@users.noreply.github.com>
2021-02-27 22:10:33 -0700

    Update GitDemo.java

--- GitDemo.java
+++ GitDemo.java
@@ -1,5 +1,5 @@
 /*****
- * Edit this line to have your name.
+ * Stephanie Tripodi
  ******/
 public class GitDemo {
     
@@ -14,14 +14,15 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        previousNumber = previousNumber+num;
+        return previousNumber;
     }
     
     /*****
      * This function should calculate the factorial of 'num' and return the resulting value.
      * i.e. if num = 4,
      *  the function will return the value of (1 * 2 * 3 * 4)
-     *****/
+     *****/  
     public double findFactorial(int num) {
         double factorialNumber = 0;
         
@@ -39,9 +40,9 @@ public class GitDemo {
      * i.e. if num = 1234
      *  the function will return 4321
      *****/
+    
     public double reverseNumber(int num){
-        double newNumber = 1;
-        
+        reverseNum=newNumber;
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
